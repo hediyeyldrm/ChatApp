5c3200a1e4b39b682ffc0d916f24a9d7aa97735d
hediyeyldrm <hediyeyldrm0@gmail.com>
2021-10-02 15:42:59 +0300

    firebase integration

---
+++ app/src/main/java/com/hediyeyildirim/chatapp/SignUpActivity.java
@@ -0,0 +1,17 @@
+package com.hediyeyildirim.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
