7e1ceb34b4ee53549ab407e7adb7ae24dd4d3da6
hediyeyldrm <hediyeyldrm0@gmail.com>
2021-10-04 14:47:31 +0300

    sign up activity design

--- app/src/main/java/com/hediyeyildirim/chatapp/SignUpActivity.java
+++ app/src/main/java/com/hediyeyildirim/chatapp/SignUpActivity.java
@@ -3,15 +3,28 @@ package com.hediyeyildirim.chatapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
-
+    EditText emailText, passwordText;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        emailText = findViewById(R.id.user_email_edit_text);
+        passwordText = findViewById(R.id.user_password_edit_text);
+
     }
+     public void signIn(View view){
+
+     }
+
+     public void signUp(View view){
+
+     }
 }
