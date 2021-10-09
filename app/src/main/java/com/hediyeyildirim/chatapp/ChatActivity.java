3b1f829fb5903e7690810e449140abea52a097df
hediyeyldrm <hediyeyldrm0@gmail.com>
2021-10-09 23:06:04 +0300

    menu operations

--- app/src/main/java/com/hediyeyildirim/chatapp/ChatActivity.java
+++ app/src/main/java/com/hediyeyildirim/chatapp/ChatActivity.java
@@ -1,14 +1,53 @@
 package com.hediyeyildirim.chatapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ChatActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.option_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.options_menu_sign_out){
+
+            mAuth.signOut();
+            Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+            startActivity(intent);
+
+        } else if (item.getItemId() == R.id.options_menu_profile){
+            Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+            startActivity(intent);
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        mAuth = FirebaseAuth.getInstance();
+
     }
 }
