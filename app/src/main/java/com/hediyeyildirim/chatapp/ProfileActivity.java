076064f5006d8f227f1d6665166f1a1e6a126ce4
hediyeyldrm <hediyeyldrm0@gmail.com>
2021-10-21 19:52:52 +0300

    profile design

--- app/src/main/java/com/hediyeyildirim/chatapp/ProfileActivity.java
+++ app/src/main/java/com/hediyeyildirim/chatapp/ProfileActivity.java
@@ -3,12 +3,32 @@ package com.hediyeyildirim.chatapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    EditText ageText;
+    ImageView userImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        ageText = findViewById(R.id.ageText);
+        userImageView = findViewById(R.id.userImageView);
+
+    }
+
+    public void upload(View view){
+
+    }
+
+    public void selectPicture(View view){
+
+
+
     }
 }
