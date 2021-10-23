f4308690feae80e8d6e82f96279cc0cb8c7e0d77
hediyeyldrm <hediyeyldrm0@gmail.com>
2021-10-23 15:01:33 +0300

    picture selection processes

--- app/src/main/java/com/hediyeyildirim/chatapp/ProfileActivity.java
+++ app/src/main/java/com/hediyeyildirim/chatapp/ProfileActivity.java
@@ -1,16 +1,30 @@
 package com.hediyeyildirim.chatapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+
+import java.io.IOException;
 
 public class ProfileActivity extends AppCompatActivity {
 
     EditText ageText;
     ImageView userImageView;
+    Uri selected;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +41,46 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     public void selectPicture(View view){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        } else {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent,2);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        if (requestCode == 1){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent,2);
+            }
+        }
 
 
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        if (requestCode == 2 && resultCode == RESULT_OK && data != null){
+
+            selected = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),selected);
+                userImageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
